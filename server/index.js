31b91cd32e780019c9cadf1cbb77905ae2b2e62a
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-15 12:59:01 -1000

    Implement get route for single product

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+const { findOne } = require('../database/helpers');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  findOne(id, (results) => {
+    res.send(results);
+  });
 });
 
 app.listen(port, () => {
