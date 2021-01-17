722767688aa6ea463581fa9f62e52a50c14d5e11
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-16 22:37:54 -1000

    Refactor server, rename database test

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const { findOne } = require('../database/helpers');
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -12,10 +11,10 @@ app.get('/', (req, res) => {
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   findOne(id, (results) => {
-    res.send(results);
+    res.status(200).send(results);
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = {
+  app,
+};
