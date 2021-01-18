ea5d11019cbbfe2ac08cca2e7832b0c3ceb0ecb6
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-18 11:40:30 -1000

    Install cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const { findOne } = require('../database/helpers');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
