75c4800f083a4a3aa8040782b005b166f22e9277
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-17 20:48:25 -1000

    Refactor server test

--- server/index.test.js
+++ server/index.test.js
@@ -2,9 +2,16 @@ const request = require('supertest');
 const { app } = require('./index');
 
 describe('GET /products/:id', () => {
-  test('It should respond with a product object', async () => {
-    const response = await request(app).get('/products/60021bc40276d4416e8f8cf3');
-    expect(response.statusCode).toBe(200);
-    // expect(response.body._id).toBe('60021bc40276d4416e8f8cf3');
+  test('It should respond with a product object', async (done) => {
+    // const response = await request(app).get('/products/60021bc40276d4416e8f8cf3');
+    // expect(response.statusCode).toBe(200);
+    // // expect(response.body._id).toBe('60021bc40276d4416e8f8cf3');
+    request(app)
+      .get('/products/60021bc40276d4416e8f8cf3')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body._id).toBe('60021bc40276d4416e8f8cf3');
+        done();
+      })
   });
 });
