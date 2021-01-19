a3b96dd54655b0101874e8b376ca06adbfe541d6
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-18 17:45:36 -1000

    Create components folder, App.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
+  getProduct() {
+    $.get('/products/60021bc40276d4416e8f8cf3', (data) => {
+      this.setState(data);
+    })
+  }
+
+  render() {
+    return <div>Hello from App.jsx</div>
+  }
+}
+
+export default App;
