605852339ee3479b8504ad49d98b073383f81123
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-19 18:23:37 -1000

    implement Returns component

---
+++ client/src/components/Returns.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Returns = () => (
+  <div id="returns">
+    <h4>15-DAY FREE & EASY RETURNS</h4>
+    <p>
+      Guest shoppers get 15 days to return or exchange
+      this item. Log in for personalized information.
+      <a href="http://localhost:3000">Learn more &gt;</a>
+    </p>
+  </div>
+);
+
+export default Returns;
