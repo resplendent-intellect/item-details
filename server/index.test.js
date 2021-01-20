8fdf8f1085b228c8636342c3206f6c172711a206
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-20 13:17:09 -1000

    use new id for test

--- server/index.test.js
+++ server/index.test.js
@@ -3,9 +3,9 @@ const { app } = require('./index');
 
 describe('GET /products/:id', () => {
   test('It should respond with a product object', async (done) => {
-    const response = await request(app).get('/products/60021bc40276d4416e8f8cf3');
+    const response = await request(app).get('/products/6007a3853fdca13bc5b1a2e5');
     expect(response.statusCode).toBe(200);
-    // expect(response.body._id).toBe('60021bc40276d4416e8f8cf3');
+    // expect(response.body._id).toBe('6007a3853fdca13bc5b1a2e5');
     done();
   });
 });
