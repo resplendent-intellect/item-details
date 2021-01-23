a2ada48de17493191bc0103d865242072d1e87b4
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-22 21:39:51 -1000

    just need modals

--- client/src/components/offers-components/Offers.jsx
+++ client/src/components/offers-components/Offers.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Offers.module.css';
 
 const Offers = ({ offers }) => (
-  <div>
+  <div className={styles.offersContainer}>
     {offers.map((offer) => (
-      <div>
+      <div className={styles.offersAndType}>
         <h5>{offer.offerType}</h5>
         {offer.offers.map((item) => (
-          <div>
+          <div className={styles.offerTitle}>
             <a href="about:blank">{item.title}</a>
           </div>
         ))}
