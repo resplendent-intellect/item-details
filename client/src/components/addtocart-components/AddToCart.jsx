c41d2f4cd1a265cfc449f6cbe81270db2ae197b9
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-22 21:19:31 -1000

    just need custom checkboxes

--- client/src/components/addtocart-components/AddToCart.jsx
+++ client/src/components/addtocart-components/AddToCart.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import styles from './AddToCart.module.css';
 
 const AddToCart = () => (
-  <div>
-    <button type="button">Add to Cart</button>
-    <div>
-      <input type="checkbox" />
-      Compare
+  <div className={styles.addToCartContainer}>
+    <div className={styles.btnContainer}>
+      <button type="button" className={styles.btn}>
+        <div>
+          <img src="https://bigbrain-itemdetails.s3-us-west-1.amazonaws.com/shoppingcart.svg" alt="shopping cart" />
+        </div>
+        <div>
+          Add to Cart
+        </div>
+      </button>
     </div>
-    <div>
-      <input type="checkbox" />
-      Save
+    <div className={styles.compareSaveContainer}>
+      <div className={styles.compare}>
+        <input type="checkbox" />
+        Compare
+      </div>
+      <div className={styles.save}>
+        <input type="checkbox" />
+        Save
+      </div>
     </div>
   </div>
 );
