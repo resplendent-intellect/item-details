08eb72310d16864ec3acd9b2259b23bed62a6954
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-23 08:49:51 -1000

    Price modal complete

--- client/src/components/price-components/Price.jsx
+++ client/src/components/price-components/Price.jsx
@@ -1,47 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PriceMatch from './PriceMatch.jsx';
 import Returns from './Returns.jsx';
+import PriceModal from './PriceModal.jsx';
 import styles from './Price.module.css';
 
-const Price = ({ price }) => (
-  <div>
-    <PriceMatch />
-    <div className={styles.pricesContainer}>
-      <div className={styles.price}>
-        $
-        {price.toFixed(2)}
-      </div>
-      <div className={styles.or}>
-        Or
-      </div>
-      <div className={styles.monthlyContainer}>
-        <div className={styles.monthly}>
-          $
-          {(price / 12).toFixed(2)}
-          /mo. *
-        </div>
-        <div className={styles.suggested}>
-          suggested payments with
-          <br />
-          {' '}
-          <b>12-Month Financing</b>
-        </div>
-        <div>
-          <a href="http://localhost:3000" className={styles.showMeHow}>Show me how &gt;</a>
-        </div>
-      </div>
-    </div>
-    <Returns />
-  </div>
-);
+const Price = ({ price }) => {
+  const [showPriceModal, setShowPriceModal] = useState(false);
 
-Price.propTypes = {
-  price: PropTypes.number,
+  return (
+    <div>
+      <PriceMatch />
+      <div className={styles.pricesContainer}>
+        <div className={styles.price}>
+          $
+          {price.toFixed(2)}
+        </div>
+        <div className={styles.or}>
+          Or
+        </div>
+        <div className={styles.monthlyContainer}>
+          <div className={styles.monthly}>
+            $
+            {(price / 12).toFixed(2)}
+            /mo. *
+          </div>
+          <div className={styles.suggested}>
+            suggested payments with
+            <br />
+            {' '}
+            <b>12-Month Financing</b>
+          </div>
+          <div>
+            <a
+              href="/"
+              className={styles.showMeHow}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowPriceModal(true);
+              }}
+            >
+              Show me how &gt;
+            </a>
+            <PriceModal
+              showPriceModal={showPriceModal}
+              closePriceModal={() => { setShowPriceModal(false); }}
+            />
+          </div>
+        </div>
+      </div>
+      <Returns />
+    </div>
+
+  );
 };
 
-Price.defaultProps = {
-  price: 100,
+Price.propTypes = {
+  price: PropTypes.number.isRequired,
 };
 
 export default Price;
