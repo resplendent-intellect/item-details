5dcc77305e4170529386929a00c458a7b0cddf38
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-25 21:37:11 -1000

    add findRandom function

--- database/helpers.js
+++ database/helpers.js
@@ -9,6 +9,18 @@ const findOne = (id, callback) => {
   });
 };
 
+const findRandom = (callback) => {
+  const random = Math.floor(Math.random() * 100);
+  Product
+    .find()
+    .skip(random)
+    .limit(1)
+    .exec((err, result) => {
+      callback(err, result);
+    });
+};
+
 module.exports = {
   findOne,
+  findRandom,
 };
