89ff3ae2a5f5a65e92006efa3b17a1bcb68b1518
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-01-28 10:41:29 -1000

    grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-run');
+
+  grunt.initConfig({
+    run: {
+      options: {
+      },
+      client: {
+        cmd: 'npm',
+        args: [
+          'run',
+          'client',
+        ],
+      },
+      upload: {
+        cmd: 'node',
+        args: [
+          'upload.js',
+        ],
+      },
+    },
+  });
+
+  grunt.registerTask('default', ['run:client', 'run:upload']);
+};
